54923245a44cd02dac86b1031b7f850637532cfe
annakristin97 <annakristin97@gmail.com>
2020-07-18 17:30:21 +0000

    Laga

---
+++ EcoFashion/EcoFashion/src/test/java/com/EFApplication/EcoFashion/EcoFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EFApplication.EcoFashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
